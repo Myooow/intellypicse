aa8d89f158329f9bc43452e7c80cd6ff7c394eae
Mathieu Tillier <mathiieu.tillier@gmail.com>
2024-03-07 01:27:55 +0100

    Don't export authOptions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import UserModel from '@/lib/database/models/User';
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
     session: {
         strategy: 'jwt',
     },
